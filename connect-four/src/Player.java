61e4d530cdd52d6fb11bb9badf82a783b12c6257
Jason Hoi <jasonhoimacau@gmail.com>
2023-09-13 17:35:56 +0800

    refactor class to separated files

---
+++ connect-four/src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    private String name;
+    private GridPosition piece;
+
+    public Player(String name, GridPosition piece) {
+        this.name = name;
+        this.piece = piece;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public GridPosition getPieceColor() {
+        return this.piece;
+    }
+}
